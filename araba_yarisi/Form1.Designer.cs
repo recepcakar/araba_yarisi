93d419739b3db2f04fa0280412495cb0b0da125b
recep <rrecepcakar@gmail.com>
2024-12-13 15:47:09 +0300

    Add project files.

---
+++ araba_yarisi/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace araba_yarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.carpma = new System.Windows.Forms.PictureBox();
+            this.bizimaraba = new System.Windows.Forms.PictureBox();
+            this.araba3 = new System.Windows.Forms.PictureBox();
+            this.araba2 = new System.Windows.Forms.PictureBox();
+            this.araba1 = new System.Windows.Forms.PictureBox();
+            this.yol = new System.Windows.Forms.PictureBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl_puan = new System.Windows.Forms.Label();
+            this.btn_oyunubaslat = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_yuksekskor = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.carpma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bizimaraba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.carpma);
+            this.panel1.Controls.Add(this.bizimaraba);
+            this.panel1.Controls.Add(this.araba3);
+            this.panel1.Controls.Add(this.araba2);
+            this.panel1.Controls.Add(this.araba1);
+            this.panel1.Controls.Add(this.yol);
+            this.panel1.Location = new System.Drawing.Point(21, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 523);
+            this.panel1.TabIndex = 0;
+            // 
+            // carpma
+            // 
+            this.carpma.Image = global::araba_yarisi.Properties.Resources.explosion;
+            this.carpma.Location = new System.Drawing.Point(217, 345);
+            this.carpma.Name = "carpma";
+            this.carpma.Size = new System.Drawing.Size(100, 50);
+            this.carpma.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carpma.TabIndex = 2;
+            this.carpma.TabStop = false;
+            // 
+            // bizimaraba
+            // 
+            this.bizimaraba.Image = global::araba_yarisi.Properties.Resources.araba9;
+            this.bizimaraba.Location = new System.Drawing.Point(232, 345);
+            this.bizimaraba.Name = "bizimaraba";
+            this.bizimaraba.Size = new System.Drawing.Size(69, 134);
+            this.bizimaraba.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bizimaraba.TabIndex = 1;
+            this.bizimaraba.TabStop = false;
+            // 
+            // araba3
+            // 
+            this.araba3.Image = global::araba_yarisi.Properties.Resources.araba5;
+            this.araba3.Location = new System.Drawing.Point(248, 92);
+            this.araba3.Name = "araba3";
+            this.araba3.Size = new System.Drawing.Size(69, 134);
+            this.araba3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.araba3.TabIndex = 1;
+            this.araba3.TabStop = false;
+            // 
+            // araba2
+            // 
+            this.araba2.Image = global::araba_yarisi.Properties.Resources.araba5;
+            this.araba2.Location = new System.Drawing.Point(407, 78);
+            this.araba2.Name = "araba2";
+            this.araba2.Size = new System.Drawing.Size(69, 134);
+            this.araba2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.araba2.TabIndex = 1;
+            this.araba2.TabStop = false;
+            // 
+            // araba1
+            // 
+            this.araba1.Image = global::araba_yarisi.Properties.Resources.araba6;
+            this.araba1.Location = new System.Drawing.Point(86, 78);
+            this.araba1.Name = "araba1";
+            this.araba1.Size = new System.Drawing.Size(69, 134);
+            this.araba1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.araba1.TabIndex = 1;
+            this.araba1.TabStop = false;
+            // 
+            // yol
+            // 
+            this.yol.Image = global::araba_yarisi.Properties.Resources.yol;
+            this.yol.Location = new System.Drawing.Point(-1, 0);
+            this.yol.Name = "yol";
+            this.yol.Size = new System.Drawing.Size(513, 775);
+            this.yol.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yol.TabIndex = 0;
+            this.yol.TabStop = false;
+            this.yol.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(214, 543);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(65, 27);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Puan:";
+            // 
+            // lbl_puan
+            // 
+            this.lbl_puan.AutoSize = true;
+            this.lbl_puan.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_puan.Location = new System.Drawing.Point(285, 543);
+            this.lbl_puan.Name = "lbl_puan";
+            this.lbl_puan.Size = new System.Drawing.Size(22, 27);
+            this.lbl_puan.TabIndex = 1;
+            this.lbl_puan.Text = "0";
+            // 
+            // btn_oyunubaslat
+            // 
+            this.btn_oyunubaslat.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_oyunubaslat.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_oyunubaslat.Location = new System.Drawing.Point(147, 585);
+            this.btn_oyunubaslat.Name = "btn_oyunubaslat";
+            this.btn_oyunubaslat.Size = new System.Drawing.Size(280, 68);
+            this.btn_oyunubaslat.TabIndex = 2;
+            this.btn_oyunubaslat.Text = "Oyunu baslat";
+            this.btn_oyunubaslat.UseVisualStyleBackColor = false;
+            this.btn_oyunubaslat.Click += new System.EventHandler(this.btn_oyunubaslat_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 551);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Yüksek skor:";
+            // 
+            // lbl_yuksekskor
+            // 
+            this.lbl_yuksekskor.AutoSize = true;
+            this.lbl_yuksekskor.Location = new System.Drawing.Point(12, 567);
+            this.lbl_yuksekskor.Name = "lbl_yuksekskor";
+            this.lbl_yuksekskor.Size = new System.Drawing.Size(14, 16);
+            this.lbl_yuksekskor.TabIndex = 3;
+            this.lbl_yuksekskor.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 677);
+            this.Controls.Add(this.lbl_yuksekskor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_oyunubaslat);
+            this.Controls.Add(this.lbl_puan);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Araba Yarisi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.carpma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bizimaraba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.araba1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox yol;
+        private System.Windows.Forms.PictureBox bizimaraba;
+        private System.Windows.Forms.PictureBox araba2;
+        private System.Windows.Forms.PictureBox araba1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl_puan;
+        private System.Windows.Forms.Button btn_oyunubaslat;
+        private System.Windows.Forms.PictureBox carpma;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_yuksekskor;
+        private System.Windows.Forms.PictureBox araba3;
+    }
+}
+
---
+++ araba_yarisi/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace araba_yarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        
+        int kazanlian_puan = 0;
+        int yol_hizi = 5;
+        int araba_hizi = 15; //benim araba hizi
+
+        bool sol_yon=false;
+        bool sag_yon = false;
+        
+        int diger_araba_hizi = 5;
+
+        Random rdr=new Random();
+        
+         
+   
+        public void oyunubaslat()
+        {
+            
+            sesac();
+            btn_oyunubaslat.Enabled = false;
+            carpma.Visible = false;
+
+            // arabanın koordinatı kendi arabam
+
+            lbl_yuksekskor.Text = Settings1.Default.yuksekskor; 
+
+            bizimaraba.Left = 160;
+            bizimaraba.Top = 300;
+
+            araba1.Left = 30; // diğğer arabaların başlangıc koordinatşarı
+            araba1.Top = 50;
+
+            araba2.Left = 320;
+            araba2.Top = 50;
+
+            sol_yon = false;
+            sag_yon = false;
+
+            carpma.Left = 195;
+            carpma.Top = 294;
+
+            timer1.Start();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kazanlian_puan ++;
+            lbl_puan.Text = kazanlian_puan.ToString();
+            if (kazanlian_puan == 1000)
+            {
+                araba3.Visible = true;
+                araba3.Enabled = true;
+             
+            }
+            if (kazanlian_puan == 2000)
+            {
+
+
+            }
+            yol.Top += yol_hizi;
+            if (yol.Top >= 400)
+            {
+                yol.Top = -100;
+            }
+
+            if (sol_yon )
+            {
+                bizimaraba.Left -= araba_hizi;
+            }
+            if (sag_yon)
+            {
+                bizimaraba.Left += araba_hizi;
+            }
+            if (bizimaraba.Left < 1) {  sol_yon = false;}
+            else if(bizimaraba.Left + bizimaraba.Width > 510) {  sag_yon=false;}
+
+            araba1.Top += diger_araba_hizi;
+            araba2.Top += diger_araba_hizi;
+            if (araba3.Enabled == true)
+            {
+                araba3.Top += diger_araba_hizi;
+                if (araba3.Top > panel1.Height)
+                {
+                    araba3degistir();
+                    araba3.Left = rdr.Next(160, 250);
+                    araba3.Top = rdr.Next(150, 250) * -1;
+                }
+            }
+
+            if (araba1.Top > panel1.Height) 
+            {
+                araba1degistir();
+                araba1.Left= rdr.Next(20,50);
+                araba1.Top= rdr.Next(40,140)*-1;
+            }
+            if (araba2.Top > panel1.Height)
+            { if(araba3.Enabled)
+                araba2degistir();
+                araba2.Left = rdr.Next(90, 250 );
+                araba2.Top = rdr.Next(40, 140) * -1;
+            }
+            if (bizimaraba.Bounds.IntersectsWith(araba1.Bounds) || bizimaraba.Bounds.IntersectsWith(araba2.Bounds) || bizimaraba.Bounds.IntersectsWith(araba3.Bounds))
+            {
+                oyunbitti();
+            }
+
+          
+        }
+        private void araba1degistir()
+        {
+            int sıra=rdr.Next(1,8);
+            switch (sıra) 
+            {
+                case 1: araba1.Image = Properties.Resources.araba2; break;
+                case 2: araba1.Image = Properties.Resources.araba3; break;
+                case 3: araba1.Image = Properties.Resources.araba4; break;
+                case 4: araba1.Image = Properties.Resources.araba5; break;
+                case 5: araba1.Image = Properties.Resources.araba6; break;
+                case 6: araba1.Image = Properties.Resources.araba7; break;
+                case 7: araba1.Image = Properties.Resources.araba8; break;
+
+            }
+
+        }
+        private void araba2degistir()
+        {
+            int sıra = rdr.Next(1, 8);
+            switch (sıra)
+            {
+                case 1: araba2.Image = Properties.Resources.araba2; break;
+                case 2: araba2.Image = Properties.Resources.araba3; break;
+                case 3: araba2.Image = Properties.Resources.araba4; break;
+                case 4: araba2.Image = Properties.Resources.araba5; break;
+                case 5: araba2.Image = Properties.Resources.araba6; break;
+                case 6: araba2.Image = Properties.Resources.araba7; break;
+                case 7: araba2.Image = Properties.Resources.araba8; break;
+
+            }
+        }
+        private void araba3degistir()
+        {
+
+            {
+                int sıra = rdr.Next(1, 8);
+                switch (sıra)
+                {
+                    case 1: araba3.Image = Properties.Resources.araba2; break;
+                    case 2: araba3.Image = Properties.Resources.araba3; break;
+                    case 3: araba3.Image = Properties.Resources.araba4; break;
+                    case 4: araba3.Image = Properties.Resources.araba5; break;
+                    case 5: araba3.Image = Properties.Resources.araba6; break;
+                    case 6: araba3.Image = Properties.Resources.araba7; break;
+                    case 7: araba3.Image = Properties.Resources.araba8; break;
+
+                }
+            }
+        }
+        private void oyunbitti() 
+        { 
+            timer1.Stop();
+            if (Convert.ToInt32(lbl_puan.Text) > Convert.ToInt32( Settings1.Default.yuksekskor))
+            {
+                Settings1.Default.yuksekskor = lbl_puan.Text;
+                
+            }
+            btn_oyunubaslat.Enabled = true;
+            carpma.Visible = true;
+            bizimaraba.Controls.Add(carpma);
+            carpma.Location = new Point(-10,-5);
+           
+            carpma.BringToFront();
+            carpma.BackColor = Color.Transparent;
+            seskapa();
+            MessageBox.Show("KAZANALIAN PUAN : "+lbl_puan.Text,"Bilgilendirme",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            btn_oyunubaslat.Enabled =true ;
+           
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            araba3.Visible = false;
+            araba3.Enabled = false;
+            oyunubaslat();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && bizimaraba.Left > 0) { sol_yon = true; }
+            if (e.KeyCode == Keys.Right && bizimaraba.Left + bizimaraba.Width < panel1.Width) { sag_yon = true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { sol_yon = false; }
+            if (e.KeyCode == Keys.Right) { sag_yon = false; }
+
+        }
+
+        private void btn_oyunubaslat_Click(object sender, EventArgs e)
+        {
+            oyunubaslat();
+            
+        }
+        private void sesac()
+        {
+            SoundPlayer ses =new SoundPlayer();
+            string sesyol = Application.StartupPath + "//Muzik.wav";
+            ses.SoundLocation=sesyol;
+            ses.Play(); 
+        }
+        private void seskapa()
+        {
+            SoundPlayer seskapa = new SoundPlayer();
+            seskapa.Stop();
+        }
+
+      
+    }
+}
